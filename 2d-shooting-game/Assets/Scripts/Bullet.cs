fabfccd5df56c9fb6f2f534be8175d2ea1a3b3e1
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-05-25 15:48:10 +0900

    180525 UnityTutorial2DShooting

--- 2d-shooting-game/Assets/Scripts/Bullet.cs
+++ 2d-shooting-game/Assets/Scripts/Bullet.cs
@@ -6,9 +6,16 @@ public class Bullet : MonoBehaviour
 {
   public int speed = 10;
 
+  // ゲームオブジェクト生成から削除するまでの時間
+  public float lifeTime = 5;
+
   void Start()
   {
+    // ローカル座標のY軸方向に移動する
     GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+
+    // lifeTime秒後に削除
+    Destroy(gameObject, lifeTime);
   }
 
   // Update is called once per frame
